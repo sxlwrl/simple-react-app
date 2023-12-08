219ef5a01415810885c1efd1c8a5ef292f223efc
sxlwrl <vadymkazak2019@gmail.com>
2023-12-08 02:06:37 +0200

    [add] Main component

---
+++ src/components/task1/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from './Header';
+import Content from './Content';
+import Image from './Image';
+
+const Main = function () {
+    return (
+        <div className='app-main'>
+            <Header />
+            <Content />
+            <Image />
+        </div>
+    );
+};
+
+export default Main;
+
+
