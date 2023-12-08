5deaac970a7faf0f72b5516c7f3543a1c01cdb97
sxlwrl <vadymkazak2019@gmail.com>
2023-12-08 02:06:11 +0200

    [add] GoodsGallery component

---
+++ src/components/task2/goodsGallery.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GoodsCard from './goodsCard';
+
+import Apple from '../../images/apple.svg';
+import Avocado from '../../images/avocado.svg';
+import DragonFruit from '../../images/dragon.svg';
+import Mango from '../../images/mango.svg';
+import Banana from '../../images/banana.svg';
+import PassionFruit from '../../images/passion.svg';
+
+const dummyGoodsData = [
+    {
+        id: 0,
+        name: 'Avocado',
+        price: 50,
+        image: Avocado,
+    },
+    {
+        id: 1,
+        name: 'Apple',
+        price: 25,
+        image: Apple,
+    },
+    {
+        id: 2,
+        name: 'Passion fruit',
+        price: 150,
+        image: PassionFruit,
+    },
+    {
+        id: 3,
+        name: 'Dragon fruit',
+        price: 210,
+        image: DragonFruit,
+    },
+    {
+        id: 4,
+        name: 'Banana',
+        price: 65,
+        image: Banana,
+    },
+    {
+        id: 5,
+        name: 'Mango',
+        price: 90,
+        image: Mango,
+    }
+];
+
+const GoodsGallery = function () {
+    console.log('bogdan loh');
+    return (
+        <div className='app'>
+            {dummyGoodsData.map((item) => (
+                <GoodsCard key={item.id} {...item} />
+            ))}
+        </div>
+    );
+};
+
+// ReactDOM.render(<GoodsGallery/>, document.getElementById('root'));
+
+export default GoodsGallery
+
+
+
